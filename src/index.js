9564c4c4429384cce421859339f643c90be5360a
Shredder1612 <anshumanshukla1612@gmail.com>
2026-06-06 20:39:27 +0530

    add a healthcheck endpoint

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
   override: true,
-})
+});
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 
 connectDB()
   .then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`);
-  });
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
   })
   .catch((error) => {
-  console.log(`mongoDB connection error`, error);
-})
+    console.log(`mongoDB connection error`, error);
+  });
